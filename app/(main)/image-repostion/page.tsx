fbada850671bc0a05b6a1f5fced6890d5f5d4cf3
Ashikur Rahman <ashikur.jebu111@gmail.com>
2024-12-30 00:07:52 +0600

    finsihed the image reposition animation and redme updated

--- app/(main)/image-repostion/page.tsx
+++ app/(main)/image-repostion/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   motion,
   useMotionValueEvent,
@@ -15,34 +15,63 @@ const profileImgSize = 116;
 
 const ImageRepositionPage = () => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const { width, height } = useViewportSize();
+  const [ref, bounds] = useMeasure();
+  const [isProfileImgVisible, setIsProfileImgVisible] = useState(false);
+
   const { scrollYProgress } = useScroll({
     container: containerRef,
     // layoutEffect: false,
   });
 
-  const { width, height } = useViewportSize();
-  const [ref, bounds] = useMeasure();
-  // const [ref, { width: containerWidth, height: containerHeight }] = useMeasure;
+  // console.log(width);
+  // console.log(height);
+  // console.log(bounds);
 
   //! Calculate the size of the image and its position
   const size = useTransform(scrollYProgress, [0, 0.15], [profileImgSize, 48]);
   const topPosition = useTransform(scrollYProgress, [0, 0.15], [6, 6]);
-  const lefPosition = useTransform(scrollYProgress, [0, 0.15], [175, 44]);
+  // const leftPosition = useTransform(scrollYProgress, [0, 0.15], [175, 44]);
+  const leftPosition = useTransform(
+    scrollYProgress,
+    [0, 0.15],
+    [
+      (width > 414 ? 414 : width) / 2 - profileImgSize / 2,
+      width > 414 ? 44 : bounds.left,
+    ]
+  );
+
+  const nameXPosition = useTransform(scrollYProgress, [0.1, 0.15], [-20, 0]);
+  const nameOpacity = useTransform(scrollYProgress, [0.1, 0.15], [0, 1]);
 
   return (
     <section className="flex justify-center items-center h-screen bg-gray-100">
       <div className="w-[480px] h-[680px] overflow-hidden relative bg-white shadow-sm">
-        <div className="w-full h-16 absolute  top-0 left-0 z-10  bg-gray-300">
-          <div
-            ref={ref}
-            className="w-12 h-12 rounded-full bg-gray-400/50"
-          ></div>
+        <div className="w-full h-16 absolute top-0 left-0 z-10 bg-gray-300">
+          <div className="flex items-center gap-2.5 h-full">
+            <div
+              ref={ref}
+              className="absolute top-0 left-0 w-12 h-12 shrink-0 rounded-full bg-gray-400/50"
+            ></div>
+            <div>
+              <motion.p
+                style={{
+                  x: nameXPosition,
+                  opacity: nameOpacity,
+                }}
+                className="text-lg font-normal text-text-black"
+              >
+                Ashikur Rahman
+              </motion.p>
+            </div>
+          </div>
           <motion.div
+            layoutId="profileImg" //! layoutId is used for animation between pages
             className="absolute left-[calc(50%-58px)] top-1.5 rounded-full bg-gray-400"
             style={{
               width: size,
               height: size,
-              left: lefPosition,
+              left: leftPosition,
               top: topPosition,
             }}
           >
@@ -60,8 +89,25 @@ const ImageRepositionPage = () => {
           ref={containerRef}
           className="w-full h-full absolute top-0 left-0 p-6 overflow-y-auto bg-blue-100"
         >
-          <div className="w-full h-[1500px] bg-orange-600/50"></div>
+          <div className="w-full h-[1500px] flex justify-center items-center bg-orange-600/50">
+            <button
+              className="bg-cyan-500 p-4 rounded-md"
+              onClick={() => setIsProfileImgVisible(true)}
+            >
+              View Profile
+            </button>
+          </div>
         </div>
+        {isProfileImgVisible && (
+          <div className="absolute top-0 left-0 size-full flex justify-center items-center bg-black/50">
+            <button
+              className="bg-lime-200 p-4 rounded-md"
+              onClick={() => setIsProfileImgVisible(false)}
+            >
+              Close
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
