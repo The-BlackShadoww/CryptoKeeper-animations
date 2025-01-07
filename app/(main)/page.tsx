7b53ccd4cd96c0388e464eb685d670f5919840f3
Ashikur Rahman <ashikur.jebu111@gmail.com>
2025-01-07 22:00:26 +0600

    modified home page and added auto hiding navigation

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,39 +1,28 @@
-import { ButtonForHeader } from "@/components/ui/button";
-import Image from "next/image";
+import { pages } from "@/constant";
+import Link from "next/link";
 
 const MainPage = () => {
-    return (
-        <section className="text-white">
-            <div className="tab:mt-[200px] mt-[96px] grid tab:grid-cols-2 grid-cols-1 tab:gap-0 gap-12">
-                <div className="space-y-[36px] tab:text-left tab:block flex flex-col justify-center items-center text-center">
-                    <h1 className="">
-                        The Future of <br /> Digital Wallets
-                        <br /> Starts{" "}
-                        <span className="text-primaryColor">Here.</span>
-                    </h1>
-                    <p className="text-[#eaeaea]/50 text-[18px] leading-[150%]">
-                        Lorem ipsum dolor sit amet consectetur adipiscing elit
-                        at
-                        <br />
-                        pharetra nec sed erat non metus suspendisse mus non{" "}
-                        <br />
-                        lectus vel vitae massa id in in turpis posuere laoreet
-                        tortor.
-                    </p>
-                    <ButtonForHeader title="Download" />
-                </div>
-                <div className="flex justify-center items-center">
-                    <Image
-                        src={"/wallet-1.png"}
-                        width={668}
-                        height={668}
-                        alt="walltet-1"
-                    />
-                </div>
-            </div>
-            <div className="h-screen w-full bg-gray-900">hi</div>
-        </section>
-    );
+  return (
+    <section className="text-white">
+      <div className="max-w-[560px] bg-[#ffffff]/[2%] mx-auto border border-white/20 rounded-lg mt-[15vh] p-6">
+        <h1 className="text-[36px] font-bold pb-4 mb-4 border-b border-white/20 text-white/50">
+          Animations
+        </h1>
+        <ul className="space-y-2">
+          {pages.map((page) => (
+            <li key={page?.title} className="flex gap-x-3">
+              <Link
+                href={page?.url ?? ""}
+                className="text-white/50 hover:text-blue-700 transition-all ease-in-out duration-150 capitalize"
+              >
+                {page?.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default MainPage;
---
+++ constant/index.ts
@@ -0,0 +1,20 @@
+export const pages = [
+  { title: "Image repostion", url: "http://localhost:3000/image-repostion" },
+  { title: "banner animation", url: "http://localhost:3000/banner-animation" },
+  { title: "card parallax", url: "http://localhost:3000/card-parallax" },
+  { title: "curved menu", url: "http://localhost:3000/curved-menu" },
+  {
+    title: "infinite text-move",
+    url: "http://localhost:3000/infinite-text-move",
+  },
+  { title: "mouse scale-img", url: "http://localhost:3000/mouse-scale-img" },
+  { title: "paralax", url: "http://localhost:3000/paralax" },
+  { title: "smooth scroll", url: "http://localhost:3000/smooth-scroll" },
+  { title: "smooth parallax", url: "http://localhost:3000/smooth-parallax" },
+  { title: "sticky footer", url: "http://localhost:3000/sticky-footer" },
+  { title: "text mask", url: "http://localhost:3000/text-mask" },
+  {
+    title: "Auto-hiding sticky navigation",
+    url: "http://localhost:3000/auto-hiding-sticky-navigation",
+  },
+];
